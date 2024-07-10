9974517172816b69a58dc3e19897bbf830a816af
Erik Grundy <erik.grundy@alstefgroup.com>
2024-07-11 10:14:04 +1200

    tidy nix files

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { system = system; };
+      pkgs = import nixpkgs { inherit system; };
     in {
       devShells.${system}.default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
